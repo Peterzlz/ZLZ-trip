44092a264939aceaba884c95e539e5ec7ee7f840
Peterzlz <liangzhizhao01@gmail.com>
2023-09-18 22:49:12 +0800

    修改了component.d.ts文件

--- components.d.ts
+++ components.d.ts
@@ -10,17 +10,10 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TabBar: typeof import('./src/components/tab_bar/TabBar.vue')['default']
-    VanButton: typeof import('vant/es')['Button']
     VanCalendar: typeof import('vant/es')['Calendar']
-    VanCell: typeof import('vant/es')['Cell']
-    VanIndexAnchor: typeof import('vant/es')['IndexAnchor']
-    VanIndexBar: typeof import('vant/es')['IndexBar']
-    VanSearch: typeof import('vant/es')['Search']
     VanSwipe: typeof import('vant/es')['Swipe']
     VanSwipeItem: typeof import('vant/es')['SwipeItem']
-    VanTab: typeof import('vant/es')['Tab']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
-    VanTabs: typeof import('vant/es')['Tabs']
   }
 }
