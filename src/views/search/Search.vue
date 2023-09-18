249420c7ae8fd5ae495fa54c55ab4f6ad9f9ef2e
peterzlz <liangzhizhao01@gmail.com>
2023-09-18 16:54:23 +0800

    search页面新增了返回上层路由的按钮

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,15 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+function btnClick() {
+  router.back()
+}
+</script>
 
 <template>
   <div class="search">
     <h2>入店时间：{{ $route.query.startDate }}</h2>
     <h2>离店时间：{{ $route.query.leaveDate }}</h2>
     <h2>城市：{{ $route.query.selectCity }}</h2>
+    <button class="back-home" @click="btnClick">返回Home页面</button>
   </div>
 </template>
 
-<style lang="less" scoped>
-.search {
-  color: red;
-}
-</style>
+<style lang="less" scoped></style>
